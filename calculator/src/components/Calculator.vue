0e9ae0ff92d84c2a70a58725615155431000b90f
vikrant <vikrant.negi74@gmail.com>
2022-01-12 22:13:20 +0530

    Calculator Structure

--- calculator/src/components/Calculator.vue
+++ calculator/src/components/Calculator.vue
@@ -10,6 +10,36 @@
         <div class="result"> {{ current || 0 }}</div>
       </div>
     </div>
+    <div class="buttonsArea">
+      <div class="buttonColumn">
+        <div @click="clear" class="buttonCalc clearButton">c</div>
+        <div @click="pushNumber('7')" class="buttonCalc">7</div>
+        <div @click="pushNumber('4')" class="buttonCalc">4</div>
+        <div @click="pushNumber('1')" class="buttonCalc">1</div>
+        <div class="buttonCalc"></div>
+      </div>
+      <div class="buttonColumn">
+        <div @click="backSpace" class="buttonCalc backButton"><i class="fas fa-backspace"></i></div>
+        <div @click="pushNumber('8')" class="buttonCalc">8</div>
+        <div @click="pushNumber('5')" class="buttonCalc">5</div>
+        <div @click="pushNumber('2')" class="buttonCalc">2</div>
+        <div @click="pushNumber('0')" class="buttonCalc">0</div>
+      </div>
+      <div class="buttonColumn">
+        <div @click="percentage" class="buttonCalc operation"><i class="fas fa-percentage"></i></div>
+        <div @click="pushNumber('9')" class="buttonCalc">9</div>
+        <div @click="pushNumber('6')" class="buttonCalc">6</div>
+        <div @click="pushNumber('3')" class="buttonCalc">3</div>
+        <div @click="pushNumber('.')" class="buttonCalc">.</div>
+      </div>
+      <div class="buttonColumn">
+        <div @click="divide" class="buttonCalc operation"><i class="fas fa-divide"></i></div>
+        <div @click="multiply" class="buttonCalc operation" ><i class="fas fa-times"></i></div>
+        <div @click="subtract" class="buttonCalc operation"><i class="fas fa-minus"></i></div>
+        <div @click="add" class="buttonCalc operation"><i class="fas fa-plus"></i></div>
+        <div @click="finalResult" class="buttonCalc operation equalButton"><i class="fas fa-equals"></i></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -68,5 +98,5 @@ export default {
     color: #FFFFFF;
     margin-bottom: 12px;
   }
-  
+
 </style>
